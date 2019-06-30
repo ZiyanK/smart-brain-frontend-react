49298673dfabd1aa507b569aab7aca471a727a24
ZiyanK <ziyankarmali786@gmail.com>
2019-06-30 13:13:45 +0530

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
     }
 
     onSumbitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://tranquil-wave-85569.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
